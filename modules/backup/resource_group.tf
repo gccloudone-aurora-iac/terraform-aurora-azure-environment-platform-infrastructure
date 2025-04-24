90aaa125c1c34fc0a683771debb13196244c983f
William Hearn <william.hearn@ssc-spc.gc.ca>
2025-04-23 22:10:47 -0400

    feat(naming): Add project to name spec

--- modules/backup/resource_group.tf
+++ modules/backup/resource_group.tf
@@ -3,7 +3,7 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 #
 resource "azurerm_resource_group" "backup" {
-  name     = "${module.azure_resource_names.resource_group_name}-backup"
+  name     = "${module.azure_resource_names.resource_group_backup_name}"
   location = var.azure_resource_attributes.location
   tags     = var.tags
 
--- modules/platform/resource_group.tf
+++ modules/platform/resource_group.tf
@@ -3,7 +3,7 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 #
 resource "azurerm_resource_group" "platform" {
-  name     = "${module.azure_resource_names.resource_group_name}-platform"
+  name     = "${module.azure_resource_names.resource_group_platform_name}"
   location = var.azure_resource_attributes.location
   tags     = var.tags
 }
