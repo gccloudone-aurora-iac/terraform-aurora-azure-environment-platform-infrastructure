5e9891865b44c94907f5247e9295721dfe1382e5
Michael Patsula <michaelpatsula@michaels-air.home>
2026-05-06 06:47:29 -0400

    feat: add API permissions to argo-workflows & grafana

--- service_principals.tf
+++ service_principals.tf
@@ -7,7 +7,7 @@
 # https://github.com/gccloudone-aurora-iac/terraform-azure-service-principal
 #
 module "argo_workflow_sso_sp" {
-  source = "git::https://github.com/gccloudone-aurora-iac/terraform-azure-service-principal.git?ref=v2.0.1"
+  source = "git::https://github.com/gccloudone-aurora-iac/terraform-azure-service-principal.git?ref=v2.1.0"
 
   azure_resource_attributes = var.azure_resource_attributes
   naming_convention         = var.naming_convention
@@ -47,7 +47,7 @@ module "argo_workflow_sso_sp" {
 # https://github.com/gccloudone-aurora-iac/terraform-azure-service-principal
 #
 module "kubecost_sp" {
-  source = "git::https://github.com/gccloudone-aurora-iac/terraform-azure-service-principal.git?ref=v2.0.1"
+  source = "git::https://github.com/gccloudone-aurora-iac/terraform-azure-service-principal.git?ref=v2.1.0"
 
   azure_resource_attributes = var.azure_resource_attributes
   naming_convention         = var.naming_convention
@@ -75,7 +75,7 @@ module "kubecost_sp" {
 # https://github.com/gccloudone-aurora-iac/terraform-azure-service-principal
 #
 module "grafana_sso_sp" {
-  source = "git::https://github.com/gccloudone-aurora-iac/terraform-azure-service-principal.git?ref=v2.0.1"
+  source = "git::https://github.com/gccloudone-aurora-iac/terraform-azure-service-principal.git?ref=v2.1.0"
 
   azure_resource_attributes = var.azure_resource_attributes
   naming_convention         = var.naming_convention
